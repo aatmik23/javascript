21befaf4b4a46b3acb849a5d9fa184d3e1e05fcf
Aatmik <108611998+aatmik23@users.noreply.github.com>
2025-09-27 13:54:17 +0000

    first done

---
+++ basics/script.js
@@ -0,0 +1,11 @@
+const accoundid = 1245
+let accname = "oliver"
+var accountpass ="auk1233"
+accountcity = 'JAIPUR'
+
+
+accname = "aleex"
+accountpass = "123oli"
+accountcity="pune"
+
+console.table([accname,accountpass,accountcity])
