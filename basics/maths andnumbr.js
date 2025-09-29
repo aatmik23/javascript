9735654926ab900ebcd3a8d6bdf5e1feba393942
Aatmik <108611998+aatmik23@users.noreply.github.com>
2025-09-29 14:33:56 +0000

    added

---
+++ basics/maths andnumbr.js
@@ -0,0 +1,19 @@
+const num = 400
+const num2 = new Number(100)
+// console.log(num2)
+// console.log(num2.toString().length)
+// console.log(num2.toFixed(2))
+let num3 = 234.78
+// console.log(num3.toPrecision(4))
+const hundreds = 1000000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+// ________________maths____________________________
+
+// console.log(Math.abs(-4)) //negative to postive
+// console.log(Math.round(4.45))
+// console.log(Math.ceil(4.2)) //top value
+// console.log(Math.floor(4.89)) //low value4
+let max=20,min=10
+let a =  Math.floor(Math.random()*(max-min+1)+min)
+console.log(a)
