b26dfa8cb7ea432121df843e33ebaf4319204ae1
Aatmik <108611998+aatmik23@users.noreply.github.com>
2025-10-06 19:29:20 +0000

    dom

---
+++ basics/dom/script.js
@@ -0,0 +1,13 @@
+//document.getelemtbyid("id")
+// .innertext se text milt 
+// .textcontent se hidden text bhi milt
+// .innerhtml html bhi dedeta
+
+//document.getelementbyyclassname('"")
+
+//.queryselector("") firsstelemt
+
+// .querrselectorall(' ')  //give node so tell which element to select [0]  use foreach
+
+//selectall (nodelist - foreach can use )and byclassnam (html collection )are not array to covert it into array use 
+// Array.from()
